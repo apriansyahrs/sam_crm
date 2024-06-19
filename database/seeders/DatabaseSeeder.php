ba7a4240fc5938e11a4482162df504ebedd6533f
apriansyahrs <apriansyahrizs@gmail.com>
2024-06-19 11:53:06 +0700

    make master-data include business entity, division, region, and cluster

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Artisan;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        $this->call([
+            RoleSeeder::class,
+            UserSeeder::class,
+        ]);
+
+        Artisan::call('shield:super-admin', ['--user' => 1]);
+        Artisan::call('shield:generate --all');
+    }
+}
